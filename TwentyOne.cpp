e176a126a3fe8a61ac439589f9305ff67d5b5ebe
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-04-13 00:54:39 +0530

    Added Delay function

--- TwentyOne.cpp
+++ TwentyOne.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<cstdlib>
-#include<ctime>
-
+#include<time.h>
 using namespace std;
+void delay(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
 main(){
     char ch;
     int count=0;
@@ -15,22 +19,85 @@ for(int i=0;i<3;i++){
 
     }
     count++;
+    delay(300);
     cout<<count;
 }
 if(count==1){
         cout<<endl<<"Cpu's Turn"<<endl;
+        delay(300);
+        cout<<++count;
+        delay(300);
+        cout<<++count;
+        delay(300);
+        cout<<++count;}
+if(count==2){
+        cout<<endl<<"Cpu's Turn"<<endl;
+        delay(300);
+        cout<<++count;
+        delay(300);
+        cout<<++count;
+        delay(300);
+       }
+ if(count==3){
+        cout<<endl<<"C";
+        delay(200);
+        cout<<"p";
+        delay(200);
+        cout<<"u";
+        delay(200);
+        cout<<"'s";
+        delay(200);
+        cout<<" ";
+        delay(200);
+        cout<<"T";
+        delay(200);
+        cout<<"u";
+        delay(200);
+        cout<<"r";
+        delay(200);
+        cout<<"n";
+        delay(200);
+        cout<<".";
+        delay(200);
+         cout<<".";
+        delay(200);
+         cout<<".";
+        delay(200);
+         cout<<".";
+        delay(200);
+
+        delay(300);
+        cout<<++count;
+        delay(300);
+        }
+
+cout<<endl<<"Your Turn"<<endl;
+for(int i=0;i<3;i++){
+    cin>>ch;
+    if(ch=='a'){
+
+        break;
+
+    }
+    count++;
+    cout<<count;
+}
+if(count==5){
+        cout<<endl<<"Cpu's Turn"<<endl;
         cout<<++count;
         cout<<++count;
         cout<<++count;}
-if(count==2){
+if(count==6){
         cout<<endl<<"Cpu's Turn"<<endl;
         cout<<++count;
         cout<<++count;
        }
- if(count==3){
+ if(count==7){
         cout<<endl<<"Cpu's Turn"<<endl;
         cout<<++count;
         }
+
+
 
 
 }
